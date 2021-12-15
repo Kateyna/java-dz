73189a6940dfb4f359661ddb058764951e5f626d
Алла <45361609+Kateyna@users.noreply.github.com>
2021-12-15 23:16:43 +0300

    Create TestAuthor.java

---
+++ practika2/Author/TestAuthor.java
@@ -0,0 +1,8 @@
+package Author;
+import java.lang.String;
+public class TestAuthor {
+    public static void main(String[] args) {
+        Author b1 = new Author("Kate", "meow12121@mail.ru" , 'w');
+        System.out.println(b1);
+    }
+}
